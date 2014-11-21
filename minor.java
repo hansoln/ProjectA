5a37093301ef6acc66280aefe1817b63c7e95215
Jens Moser <Jens.Moser@nrk.no>
2014-11-21 12:08:57 +0100

    Moved minor

---
+++ minor.java
@@ -0,0 +1,24 @@
+asdasdasdasdahsdhashd hejpublic static main(String[] args) {
+
+
+ompa
+
+Halla
+df
+sdf 
+ asdfasd asdf asdf asdf asdf
+
+sdf 
+asdf 
+asdf a
+sdf 
+
+
+asdf 
+asdf 
+asdf 
+dsaf asdf 
+asdf 
+:
+
+
